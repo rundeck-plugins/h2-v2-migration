1f82fcf198109c77ff58cd05b856377a97ea5c3d
Greg Schueler <greg.schueler@gmail.com>
2022-04-15 15:53:51 -0700

    cleanup

--- test/create_test_content.sh
+++ test/create_test_content.sh
@@ -10,10 +10,8 @@ source "${SRC_DIR}/common.sh"
 main() {
   local DATADIR=$1
 
-  echo "Authenticate"
   TOKEN=$(authenticate $DATADIR)
   [ -n "$TOKEN" ] && [ "null" != "$TOKEN" ] || die "Could not get auth TOKEN"
-  echo "Test API with token $TOKEN"
 
   #verify auth connection
   api_get "system/info" > "$DATADIR/system-info.json"
@@ -21,10 +19,10 @@ main() {
   assert_json_notnull "$DATADIR/system-info.json" ".system.rundeck.version" "System Info Version"
 
   # create 'test' project
-  api_post "projects" '{"name":"test"}' >"$DATADIR/create-project.out"
+  api_post "projects" '{"name":"test"}' >"$DATADIR/create-project.json"
   [ 0 == $? ] || die "Create project failed"
-  VAL=$(jq -r .url "$DATADIR/create-project.out")
-  VAL2=$(jq -r .errorCode "$DATADIR/create-project.out")
+  VAL=$(jq -r .url "$DATADIR/create-project.json")
+  VAL2=$(jq -r .errorCode "$DATADIR/create-project.json")
   { [ -n "$VAL" ] && [ "null" != "$VAL" ] ; } ||
     { [ -n "$VAL2" ] && [ "api.error.item.alreadyexists" == "$VAL2" ] ; } ||
     die "Could not create project: $VAL"
@@ -33,14 +31,15 @@ main() {
   VAL=$(api_jobxml_create "@${SRC_DIR}/test-job.xml" | jq -r ".succeeded[0].message + \"/\" + .skipped[0].error")
   [ 0 == $? ] || die "Create job failed"
   [ -n "$VAL" ] && [ "null" != "$VAL" ] || die "Could not get value: $VAL"
+  echo "√ Created Job"
 
   # execution test job
-  api_post "job/$JOBID/executions" '{}' >"$DATADIR/run1.out"
+  api_post "job/$JOBID/executions" '{}' >"$DATADIR/run1.json"
   [ 0 == $? ] || die "Execute job failed"
 
-  execid=$(jq -r .id "$DATADIR/run1.out")
+  execid=$(jq -r .id "$DATADIR/run1.json")
   [ -n "$execid" ] && [ "null" != "$execid" ] || die "Could not get execution id"
-  echo "Created execution: $execid"
+  echo "√ Created execution: $execid"
 
   #TODO: load webhook
 }
