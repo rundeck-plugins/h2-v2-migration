88eb148a42f64d85b32495141f1eea3daa520598
ehe-pd <ehe@pagerduty.com>
2022-04-14 19:17:27 -0700

    Migration shell script and changelog script

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # h2-v2-migration
 Migration Script and instructions for migrating from H2 V1 to V2
+
+
+# Preparation:
+
+1. Create a backup directory `${backup_directory}` in your local file system to host the backup database files
+2. Clone the git repo [h2-v2-migration](https://github.com/rundeck-plugins/h2-v2-migration) into your local file system
+
+# Backup
+Before migration - copy and backup the database files somewhere safe:
+1. Stop the Rundeck application
+2. Copy the H2 database from the Rundeck application directory `{RUNDECK_HOME}/server/data` to the `${backup_directory}` There should be two files
+        
+        grailsdb.mv.db
+        grailsdb.trace.db
+
+# Migration
+
+## STEP-1. Generate the new version database
+
+Open a shell terminal and navigate into the `h2-v2-migration` git repo. Execute the `migration.sh` shell command.
+
+    
+    $_>/bin/sh migration.sh -f ${backup_directory}/grailsdb.mv.db -u ${username} -p ${password}
+    
+
+The `-f` option is required and should be the full path to the backup database file.
+
+By default, the `username` and `password` parameters are both empty string. If you have any custom setup, please use your customized value.
+
+
+
+## STEP-2. Deploy the new version database
+- Use the generated database file `./output/v2/data/grails.mv.db` from last step to replace your the target Rundeck application database at `{RUNDECK_HOME}/server/data/grails.mv.db`
+- Set the permission of the file `{RUNDECK_HOME}/server/data/grails.mv.db` correctly, so Rundeck application can access it with write permission. Login to the docker container’s shell to change the ownership of the database files by executing the below command: 
+
+       sudo chown rundeck:root {RUNDECK_HOME}/server/data/grailsdb.mv.db
+- Restart the Rundeck application
---
+++ migration.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+
+while getopts f:u:p: flag
+do
+    case "${flag}" in
+        f) file=${OPTARG};;
+        u) username=${OPTARG};;
+        p) password=${OPTARG};;
+    esac
+done
+
+if [ -z "$file" ]; then
+    echo "===> Please use -f option to provide the path to the database file. e.g. migration.sh -f path/to/database/file.mv.db"
+    exit -1
+fi
+
+echo "===> Start to migrate h2 v1.x database from file: $file to v2."
+echo "===> Use db username: $username"
+echo "===> Use db password: $password"
+
+echo "===> Check output directory."
+
+OUTPUT_DIR=output
+
+if [ -d "$OUTPUT_DIR" ]; then
+  # Take action if $DIR exists. #
+  echo "===> $OUTPUT_DIR directory exists."
+else
+  mkdir $OUTPUT_DIR
+  echo "===> Create the $OUTPUT_DIR."
+fi
+
+if [ -d "$OUTPUT_DIR/v2" ]; then
+    echo "===> Remove generated data from last run"
+    rm -rf "$OUTPUT_DIR/v2"
+fi
+
+echo "===> Start to download JDBC drivers: "
+curl https://repo1.maven.org/maven2/com/h2database/h2/1.4.200/h2-1.4.200.jar --output $OUTPUT_DIR/h2-1.4.200.jar
+
+curl https://repo1.maven.org/maven2/com/h2database/h2/2.1.212/h2-2.1.212.jar --output $OUTPUT_DIR/h2-2.1.212.jar
+
+echo "===> Jdbc drivers downloaded."
+echo ""
+
+echo "===> Start to export database into SQL script: "
+java -cp $OUTPUT_DIR/h2-1.4.200.jar org.h2.tools.Script -url "jdbc:h2:$file" -user "" -script "./$OUTPUT_DIR/backup.sql"
+echo "===> Done export."
+echo ""
+
+echo "===> Start to create h2 v2 database from the exported SQL script: "
+java -cp $OUTPUT_DIR/h2-2.1.212.jar org.h2.tools.RunScript -url "jdbc:h2:./$OUTPUT_DIR/v2/data/grailsdb" -user "" -script "./$OUTPUT_DIR/backup.sql" -options FROM_1X
+echo "===> v2 database has been created at ./$OUTPUT_DIR/v2/data/grailsdb.mv.db"
+echo ""
+
+echo "===> Fix databsae change logs"
+java -cp $OUTPUT_DIR/h2-2.1.212.jar org.h2.tools.RunScript -url "jdbc:h2:./$OUTPUT_DIR/v2/data/grailsdb" -user "" -script "./changelog_backup.sql"
+echo "===> Database change logs has been fixed."
+echo ""
+echo ""
+
+
+echo "================================================================================================================="
+echo ""
+echo "Please copy the new database file ./$OUTPUT_DIR/v2/data/grailsdb.mv.db to your {RUNDECK_HOME}/server/data/ folder to replace the old version database"
+echo ""
+echo "Please run command \`chown rundeck:root {RUNDECK_HOME}/server/data/grailsdb.mv.db\` to set the right permission on the new database file"
+echo ""
+echo "================================================================================================================="
+
+
+
