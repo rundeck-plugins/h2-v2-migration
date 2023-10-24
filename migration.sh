025ae92ba30f3c38ed18a7397a6b3c748cd490ed
Alberto Hormazabal <ahormazabal@exatech.cl>
2023-10-24 17:51:39 -0300

    Migration from H2 2.1 to 2.2

--- migration.sh
+++ migration.sh
@@ -11,10 +11,10 @@ done
 
 if [ ! -f "$file.mv.db" ]; then
     echo "===> $file is not a valid path. Please use -f option to provide the path to the database file. e.g. migration.sh -f path/to/database/file without extension"
-    exit -1
+    exit 255
 fi
 
-echo "===> Start to migrate h2 v1.x database from file: $file to v2."
+echo "===> Start to migrate h2 v2.1 database from file: $file to v2.2"
 echo "===> Use db username: $username"
 echo "===> Use db password: $password"
 
@@ -36,33 +36,35 @@ if [ -d "$OUTPUT_DIR/v2" ]; then
 fi
 
 echo "===> Start to download JDBC drivers: "
-curl https://repo1.maven.org/maven2/com/h2database/h2/1.4.200/h2-1.4.200.jar --output $OUTPUT_DIR/h2-1.4.200.jar
+SOURCE_JAR=$OUTPUT_DIR/h2-2.1.210.jar
+DEST_JAR=$OUTPUT_DIR/h2-2.2.200.jar
+curl https://repo1.maven.org/maven2/com/h2database/h2/2.1.210/h2-2.1.210.jar --output $SOURCE_JAR
+curl https://repo1.maven.org/maven2/com/h2database/h2/2.2.220/h2-2.2.220.jar --output $DEST_JAR
 
-curl https://repo1.maven.org/maven2/com/h2database/h2/2.1.212/h2-2.1.212.jar --output $OUTPUT_DIR/h2-2.1.212.jar
 
 echo "===> Jdbc drivers downloaded."
 echo ""
 
-echo "===> Start to export database into SQL script: "
-java -cp $OUTPUT_DIR/h2-1.4.200.jar org.h2.tools.Script -url "jdbc:h2:$file" -user "$username" -password "$password" -script "./$OUTPUT_DIR/backup.sql"
+echo "===> Start to export current database into SQL script: "
+java -cp $SOURCE_JAR org.h2.tools.Script -url "jdbc:h2:$file" -user "$username" -password "$password" -script "./$OUTPUT_DIR/backup.sql"
 echo "===> Done export."
 echo ""
 
-echo "===> Start to create h2 v2 database from the exported SQL script: "
-java -cp $OUTPUT_DIR/h2-2.1.212.jar org.h2.tools.RunScript -url "jdbc:h2:./$OUTPUT_DIR/v2/data/grailsdb" -user "$username" -password "$password" -script "./$OUTPUT_DIR/backup.sql" -options FROM_1X
-echo "===> v2 database has been created at ./$OUTPUT_DIR/v2/data/grailsdb.mv.db"
+echo "===> Start to create h2 v2.2 database from the exported SQL script: "
+java -cp $DEST_JAR org.h2.tools.RunScript -url "jdbc:h2:./$OUTPUT_DIR/v2.2/data/grailsdb" -user "$username" -password "$password" -script "./$OUTPUT_DIR/backup.sql"
+echo "===> v2.2 database has been created at ./$OUTPUT_DIR/v2.2/data/grailsdb.mv.db"
 echo ""
 
-echo "===> Fix databsae change logs"
-java -cp $OUTPUT_DIR/h2-2.1.212.jar org.h2.tools.RunScript -url "jdbc:h2:./$OUTPUT_DIR/v2/data/grailsdb" -user "$username" -password "$password" -script "./changelog_backup.sql"
-echo "===> Database change logs has been fixed."
-echo ""
-echo ""
+#echo "===> Fix databsae change logs"
+#java -cp $DEST_JAR org.h2.tools.RunScript -url "jdbc:h2:./$OUTPUT_DIR/v2.2/data/grailsdb" -user "$username" -password "$password" -script "./changelog_backup.sql"
+#echo "===> Database change logs has been fixed."
+#echo ""
+#echo ""
 
 
 echo "================================================================================================================="
 echo ""
-echo "Please copy the new database file ./$OUTPUT_DIR/v2/data/grailsdb.mv.db to your {RUNDECK_HOME}/server/data/ folder to replace the old version database"
+echo "Please copy the new database file ./$OUTPUT_DIR/v2.2/data/grailsdb.mv.db to your {RUNDECK_HOME}/server/data/ folder to replace the old version database"
 echo ""
 echo "Please run command \`chown rundeck:root {RUNDECK_HOME}/server/data/grailsdb.mv.db\` to set the right permission on the new database file"
 echo ""
